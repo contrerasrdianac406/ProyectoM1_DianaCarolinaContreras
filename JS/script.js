1eb1ee4a61ecaf598759374ead1fdffaf9a918e9
contrerasrdianac406 <contrerasrdianac@gmail.com>
2026-05-07 21:35:56 -0500

    Feat[Hmtl y CSS]: Se crea el marco para las tarjetas de colores y se agregan los estilos

--- JS/script.js
+++ JS/script.js
@@ -5,14 +5,25 @@ contador = 7;
 
 function agregarCard() {
   if (contador < 10) {
+    const nuevomarco = document.createElement("div");
+    nuevomarco.setAttribute("id", "marco-tarjeta");
+    contenedorColores.appendChild(nuevomarco);
     const nuevocard = document.createElement("div");
-    nuevocard.textContent = "item" + contador;
     nuevocard.setAttribute("id", "tarjeta");
     nuevocard.classList.add("card" + contador);
-    contenedorColores.appendChild(nuevocard);
+    nuevomarco.appendChild(nuevocard);
     contador++;
   } else {
     alert("Ya no puedes crear mas colores");
   }
 }
 btnAgregarCard.addEventListener("click", agregarCard);
+
+function generarHexadecimal() {
+  const caracteresHexadecimales = "0123456789ABCDEF";
+  let hexadecimal = "#";
+  for (let i = 0; i < 6; i++) {
+    hexadecimal += caracteresHexadecimales[Math.floor(Math.random() * 16)];
+  }
+  return hexadecimal;
+}
