8aa5ced5a439a654c2e87026a7492fbf19917f41
contrerasrdianac406 <contrerasrdianac@gmail.com>
2026-05-12 20:24:28 -0500

    Feat:[JS]:se crea la funcion para cambiar el codigo de hexadecimal a HSL

--- JS/script.js
+++ JS/script.js
@@ -46,37 +46,3 @@ function generarHexadecimal() {
   return hexadecimal;
 }
 btnGenerarPaleta.addEventListener("click", GenerarPaleta);
-
-function hexToHsl(hex) {
-  // Quitamos el # y convertimos a valores R, G, B
-  let r = parseInt(hex.substring(1, 3), 16) / 255;
-  let g = parseInt(hex.substring(3, 5), 16) / 255;
-  let b = parseInt(hex.substring(5, 7), 16) / 255;
-
-  let max = Math.max(r, g, b),
-    min = Math.min(r, g, b);
-  let h,
-    s,
-    l = (max + min) / 2;
-
-  if (max === min) {
-    h = s = 0; // Acromático (gris)
-  } else {
-    let d = max - min;
-    s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < b ? 6 : 0);
-        break;
-      case g:
-        h = (b - r) / d + 2;
-        break;
-      case b:
-        h = (r - g) / d + 4;
-        break;
-    }
-    h /= 6;
-  }
-
-  return `hsl(${Math.round(h * 360)}, ${Math.round(s * 100)}%, ${Math.round(l * 100)}%)`;
-}
